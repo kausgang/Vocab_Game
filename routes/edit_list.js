aab65aa7f9914e04f99a23f9645ce0071b2dd100
kausgang <write.to.kaustav@gmail.com>
2022-11-04 01:30:50 -0400

    archived db when editing

--- routes/edit_list.js
+++ routes/edit_list.js
@@ -6,6 +6,9 @@ var path = require("path");
 var shuffle = require("shuffle-array");
 // const { copy } = require('../app');
 
+const directoryPath = path.join("./public", "DATABASE");
+const archivePath = path.join("./public", "ARCHIVE");
+
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   var file = req.query.list;
@@ -28,7 +31,46 @@ router.get("/", function (req, res, next) {
   });
 
   //delete the original list first
-  fs.unlinkSync(filename);
+  // fs.unlinkSync(filename);
+
+  // move the original list to archieve folder
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1;
+  var yyyy = today.getFullYear();
+
+  let hh = today.getHours();
+  let min = today.getMinutes();
+  let ss = today.getSeconds();
+
+  if (dd < 10) {
+    dd = "0" + dd;
+  }
+
+  if (mm < 10) {
+    mm = "0" + mm;
+  }
+  if (hh < 10) {
+    hh = "0" + hh;
+  }
+
+  if (min < 10) {
+    min = "0" + min;
+  }
+  if (ss < 10) {
+    ss = "0" + ss;
+  }
+  today = dd + "-" + mm + "-" + yyyy + "-" + hh + "-" + min + "-" + ss;
+
+  fs.rename(
+    filename,
+    path.join(archivePath, file + ".json_" + today),
+    // path.join(archivePath, file),
+    function (err) {
+      if (err) throw err;
+      console.log("Successfully renamed - AKA moved!", file);
+    }
+  );
 
   res.render("edit_list", {
     word_count: word_count,
